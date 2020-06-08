7230ca117da20c5ee07e1e845e5c5722e51d60e8
GangWoon <gangwoon2018@gmail.com>
2020-06-08 15:19:37 +0900

    [#2] feat: Init Project

---
+++ iOS/IssueTracker/IssueTracker/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  IssueTracker
+//
+//  Created by Cloud on 2020/06/08.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
