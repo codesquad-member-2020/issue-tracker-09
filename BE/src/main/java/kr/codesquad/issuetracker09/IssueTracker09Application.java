01ebec78836e85133f22a7a52e5fd4782baa0850
blossun <dnsngu@gmail.com>
2020-06-09 14:26:59 +0900

    [#4] Chore : 프로젝트 초기화
    Spring Web, MySQL, DevTools 의존성 추가

---
+++ BE/src/main/java/kr/codesquad/issuetracker09/IssueTracker09Application.java
@@ -0,0 +1,13 @@
+package kr.codesquad.issuetracker09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTracker09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTracker09Application.class, args);
+	}
+
+}
