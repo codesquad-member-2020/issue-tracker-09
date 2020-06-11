7359e838ee8ed63b2e33d0e65746dd9256b5bdbe
GangWoon <gangwoon2018@gmail.com>
2020-06-11 17:19:20 +0900

    [#18] feat: LabelTableViewController 구현
    ReusableTableViewController을 상속받고, 셀을 그리는 부분과 headerView를 그리는 부분을 구현했습니다.

---
+++ iOS/IssueTracker/IssueTracker/Supports/LabelTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LabelTableViewController.swift
+//  IssueTracker
+//
+//  Created by Cloud on 2020/06/11.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+final class LabelTableViewController: ReusableTableViewController {
+    
+    // MARK: - Properties
+    private let headerViewTitle: String = "Label"
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerCell(anyClass: LabelTableViewCell.self,
+                     identifier: LabelTableViewCell.identifier)
+    }
+    
+    // MARK: - Methods
+    // MARK: DataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: LabelTableViewCell.identifier, for: indexPath) as? LabelTableViewCell else { return LabelTableViewCell() }
+        cell.apply(title: "feature",
+                   description: "기능에 대한 레이블입니다.",
+                   backgroundColor: .green)
+        
+        return cell
+    }
+    
+    // MARK: Delegate
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: ReusableHeaderView.identifier) as? ReusableHeaderView
+        headerView?.apply(title: headerViewTitle)
+        
+        return headerView
+    }
+}
