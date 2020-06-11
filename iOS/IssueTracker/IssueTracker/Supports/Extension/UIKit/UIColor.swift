7ac2f42efe09c196a2043b526392b48f4a7bed63
GangWoon <gangwoon2018@gmail.com>
2020-06-11 18:12:29 +0900

    [#21] feat: Hex Color to  UIColor

--- iOS/IssueTracker/IssueTracker/Supports/Extension/UIKit/UIColor.swift
+++ iOS/IssueTracker/IssueTracker/Supports/Extension/UIKit/UIColor.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 extension UIColor {
+    static let mask: Int = 0x000000FF
+    static let colorPalette: CGFloat = 255.0
     
+    convenience init?(hex: String, alpha: CGFloat = 1) {
+        var color: UInt64 = 0
+        guard hex.hasPrefix("#") else { return nil }
+        let hexColor = String(hex.dropFirst())
+        guard hexColor.count == 6 else { return nil }
+        let scanner = Scanner(string: hexColor)
+        scanner.scanHexInt64(&color)
+        let red = CGFloat(Int(color >> 16) & Self.mask) / Self.colorPalette
+        let green = CGFloat(Int(color >> 8) & Self.mask) / Self.colorPalette
+        let blue = CGFloat(Int(color) & Self.mask) / Self.colorPalette
+        
+        self.init(red: red,
+                  green: green,
+                  blue: blue,
+                  alpha: alpha)
+    }
 }
--- iOS/IssueTracker/IssueTracker/Supports/LabelTableViewController.swift
+++ iOS/IssueTracker/IssueTracker/Supports/LabelTableViewController.swift
@@ -28,9 +28,11 @@ final class LabelTableViewController: ReusableTableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: LabelTableViewCell.identifier, for: indexPath) as? LabelTableViewCell else { return LabelTableViewCell() }
+        
+        guard let backgroundColor = UIColor(hex: "#e1f7d5") else { return cell }
         cell.apply(title: "feature",
                    description: "기능에 대한 레이블입니다.",
-                   backgroundColor: .green)
+                   backgroundColor: backgroundColor)
         
         return cell
     }
