572861cc4eaddc2ae4d4c9c6a86705cc7eede6ef
GangWoon <gangwoon2018@gmail.com>
2020-06-11 17:17:40 +0900

    [#18] feat: ReusableTableViewController를 상속받을 수 있도록 구현
    자식 뷰컨에서 공통적으로 사용하는 부분을 구현했습니다.

--- iOS/IssueTracker/IssueTracker/Reusable/ReusableTableViewController.swift
+++ iOS/IssueTracker/IssueTracker/Reusable/ReusableTableViewController.swift
@@ -8,21 +8,33 @@
 
 import UIKit
 
-@IBDesignable
 class ReusableTableViewController: UITableViewController {
 
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
     }
     
     // MARK: - Methods
-    // MARK: Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+    // MARK: DataSource
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return ReusableHeaderView.height
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+    
+    func registerCell(anyClass: AnyClass, identifier: String) {
+        tableView.register(anyClass.self,
+                           forCellReuseIdentifier: identifier)
+    }
+    
+    // MARK: Configure
+    private func configure() {
+        tableView.showsVerticalScrollIndicator = false
+        registerHeaderView()
+    }
+    
+    private func registerHeaderView() {
+        tableView.register(ReusableHeaderView.self,
+                           forHeaderFooterViewReuseIdentifier: ReusableHeaderView.identifier)
     }
 }
