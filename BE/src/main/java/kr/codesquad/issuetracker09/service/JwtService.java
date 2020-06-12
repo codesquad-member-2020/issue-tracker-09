1c4eca554887a367798b3baf4e0f3e508621fe5e
suhyunsim <cnc1106@naver.com>
2020-06-12 15:53:50 +0900

    [#10] feat: JwtService 구현
    jwt 토큰을 만드는 buildJwt 메서드, 파싱하는 parseJwt 메서드를 구현한다.

---
+++ BE/src/main/java/kr/codesquad/issuetracker09/service/JwtService.java
@@ -0,0 +1,64 @@
+package kr.codesquad.issuetracker09.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import kr.codesquad.issuetracker09.domain.User;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
+public class JwtService {
+
+    //TODO: 시크릿 키 암호화, 키 내용 확인
+    SecretKey key = Keys.hmacShaKeyFor("".getBytes());
+
+    public String buildJwt(User user) {
+        return Jwts.builder()
+                .setHeader(createHeaders())
+                .setClaims(createPayloads(user))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public User parseJwt(String jwt) {
+        jwt = jwt.replace("Bearer ", "");
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+        Long id = (Long) claims.get("id");
+        String name = (String) claims.get("name");
+        Long socialId = (Long) claims.get("socialId");
+        String email = (String) claims.get("email");
+        return User.builder()
+                .id(id)
+                .name(name)
+                .socialId(socialId)
+                .email(email)
+                .build();
+    }
+
+    private Map<String, Object> createHeaders() {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+        return headers;
+    }
+
+    private Map<String, Object> createPayloads(User user) {
+        Map<String, Object> payloads = new HashMap<>();
+        payloads.put("id", user.getId());
+        payloads.put("name", user.getName());
+        payloads.put("socialId", user.getSocialId());
+        payloads.put("email", user.getEmail());
+        return payloads;
+    }
+
+}
