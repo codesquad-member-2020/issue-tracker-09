554fb00d98b49752f5651953fb9ab58dbf2f7fce
blossun <dnsngu@gmail.com>
2020-06-13 21:18:56 +0900

    [#10] feat: Apple 로그인을 처리할 컨트롤러 구현
    - Apple 로그인에 성공한 User의 socialId, name, email 정보를 넘겨받는다.
    넘겨받은 정보를 이용해 JWT 토큰을 발행해서 Authorization 헤더에 담아서 반환

--- BE/src/main/java/kr/codesquad/issuetracker09/web/login/controller/LoginController.java
+++ BE/src/main/java/kr/codesquad/issuetracker09/web/login/controller/LoginController.java
@@ -3,17 +3,25 @@ package kr.codesquad.issuetracker09.web.login.controller;
 import kr.codesquad.issuetracker09.domain.User;
 import kr.codesquad.issuetracker09.service.JwtService;
 import kr.codesquad.issuetracker09.service.OAuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
+import javax.xml.ws.Response;
 import java.io.IOException;
 
 @RestController
 public class LoginController {
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
     private final JwtService jwtService;
     private final OAuthService oAuthService;
 
@@ -29,5 +37,16 @@ public class LoginController {
         String jwt = jwtService.buildJwt(user);
         response.sendRedirect("issuenine://oauth?token=" + jwt);
         return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT).body("redirect");
+    }
+
+    @PostMapping("/applelogin")
+    public ResponseEntity<String> appleLogin(@RequestBody User user) {
+        log.debug("[*] Apple user info : {}", user);
+        String jwt = "test";
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setBearerAuth(jwt);
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body("logined");
     }
 }
