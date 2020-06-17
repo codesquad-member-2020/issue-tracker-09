a389fb07117ecb72745670759a6b9632fd3dd2f1
suhyunsim <cnc1106@naver.com>
2020-06-18 02:41:06 +0900

    [#33] feat: MilestoneController에 milestone을 생성하도록 PUT 매핑

--- BE/src/main/java/kr/codesquad/issuetracker09/web/milestone/controller/MilestoneController.java
+++ BE/src/main/java/kr/codesquad/issuetracker09/web/milestone/controller/MilestoneController.java
@@ -3,10 +3,10 @@ package kr.codesquad.issuetracker09.web.milestone.controller;
 import kr.codesquad.issuetracker09.domain.Milestone;
 import kr.codesquad.issuetracker09.service.MilestoneService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Slf4j
@@ -23,5 +23,12 @@ public class MilestoneController {
     @GetMapping()
     public List<Milestone> getAllMilestones() {
         return milestoneService.findAll();
+    }
+
+    @PostMapping()
+    public void create(@RequestBody Milestone milestone, HttpServletResponse response) {
+        log.debug("[*] create - milestone : {}", milestone);
+        milestoneService.save(milestone);
+        response.setStatus(HttpStatus.CREATED.value());
     }
 }
