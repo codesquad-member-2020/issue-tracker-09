022605047aec0af0e50fe36355638263a145bd74
limwin94 <amuguna94@gmail.com>
2020-06-17 16:25:02 +0900

    [#51] feat: network protocol 구현
    IssueTrackerNetwork error도 함께 구현

---
+++ iOS/IssueTracker/IssueTracker/Network/IssueTrackerNetwork.swift
@@ -0,0 +1,30 @@
+//
+//  IssueTrackerNetwork.swift
+//  IssueTracker
+//
+//  Created by 임승혁 on 2020/06/17.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+enum IssueTrackerNetworkError: Error {
+    case error(String)
+    case defalutError
+    
+    var message: String {
+        switch self {
+        case let .error(msg):
+            return msg
+        case .defalutError:
+            return "잠시 후에 다시 시도해주세요"
+        }
+    }
+}
+
+protocol IssueTrackerNetwork {
+    var session: URLSession { get }
+    
+    func requeset<T: Decodable>(_ type: T.Type, providing: RequestPorviding) -> AnyPublisher<T, IssueTrackerNetworkError>
+}
