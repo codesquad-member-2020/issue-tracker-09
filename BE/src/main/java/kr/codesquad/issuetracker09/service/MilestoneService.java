64124919ec7f4f0b155c01374581ebc5b6b421ee
suhyunsim <cnc1106@naver.com>
2020-06-18 03:21:10 +0900

    [#33] refactor: milestone title이 null인 경우 예외처리

--- BE/src/main/java/kr/codesquad/issuetracker09/service/MilestoneService.java
+++ BE/src/main/java/kr/codesquad/issuetracker09/service/MilestoneService.java
@@ -2,6 +2,9 @@ package kr.codesquad.issuetracker09.service;
 
 import kr.codesquad.issuetracker09.domain.Milestone;
 import kr.codesquad.issuetracker09.domain.MilestoneRepository;
+import kr.codesquad.issuetracker09.exception.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -22,6 +25,9 @@ public class MilestoneService {
     }
 
     public Milestone save(Milestone milestone) {
+        if (milestone.getTitle() == null) {
+            throw new ValidationException("Title can't be blank");
+        }
         return milestoneRepository.save(milestone);
     }
 }
