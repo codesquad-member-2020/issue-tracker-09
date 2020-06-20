42be323892add4c64420e756e984b95148bd937d
suhyunsim <cnc1106@naver.com>
2020-06-20 16:35:57 +0900

    [#33] feat: Milestone picker 메서드 구현
    ModelMapper를 생성자 기반으로 주입한다.
    Milestone 엔티티 목록 전체를 가져와서 modelMapper를 통해 DTO형태로 Object Mappping 한다.
    PickerDTO의 전체 목록을 가져온다.

--- BE/src/main/java/kr/codesquad/issuetracker09/service/MilestoneService.java
+++ BE/src/main/java/kr/codesquad/issuetracker09/service/MilestoneService.java
@@ -3,21 +3,27 @@ package kr.codesquad.issuetracker09.service;
 import kr.codesquad.issuetracker09.domain.Milestone;
 import kr.codesquad.issuetracker09.domain.MilestoneRepository;
 import kr.codesquad.issuetracker09.exception.ValidationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import kr.codesquad.issuetracker09.web.milestone.dto.MilestonePickerDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
+@Transactional
 @Service
 public class MilestoneService {
 
     private MilestoneRepository milestoneRepository;
+    private ModelMapper modelMapper;
 
-    public MilestoneService(MilestoneRepository milestoneRepository) {
+    public MilestoneService(MilestoneRepository milestoneRepository, ModelMapper modelMapper) {
         this.milestoneRepository = milestoneRepository;
+        this.modelMapper = modelMapper;
     }
 
     public List<Milestone> findAll() {
@@ -38,5 +44,15 @@ public class MilestoneService {
 
     public void delete(Long milestoneId) {
         milestoneRepository.deleteById(milestoneId);
+    }
+
+    public List<MilestonePickerDTO> findAllPicker() {
+        List<Milestone> milestones = milestoneRepository.findAll();
+        List<MilestonePickerDTO> milestonePickerDTOList = new ArrayList<>();
+        for (Milestone milestone : milestones) {
+            MilestonePickerDTO milestonePickerDTO = modelMapper.map(milestone, MilestonePickerDTO.class);
+            milestonePickerDTOList.add(milestonePickerDTO);
+        }
+        return milestonePickerDTOList;
     }
 }
