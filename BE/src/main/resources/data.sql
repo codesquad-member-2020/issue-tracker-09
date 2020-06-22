fb6ffea6b4d0b4ff45e5b6681dbb77ef73887506
blossun <dnsngu@gmail.com>
2020-06-23 02:15:46 +0900

    [#65] chore: Assignee 목 데이터 추가

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -34,3 +34,7 @@ INSERT INTO issue_has_label (issue_id, label_id) VALUES (1, 2);
 INSERT INTO issue_has_label (issue_id, label_id) VALUES (1, 3);
 INSERT INTO issue_has_label (issue_id, label_id) VALUES (2, 2);
 INSERT INTO issue_has_label (issue_id, label_id) VALUES (2, 3);
+
+INSERT INTO assignee (issue_id, user_id) VALUES (1, 1);
+INSERT INTO assignee (issue_id, user_id) VALUES (1, 2);
+INSERT INTO assignee (issue_id, user_id) VALUES (2, 2);
