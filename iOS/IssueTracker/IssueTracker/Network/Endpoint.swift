b3f2047a3411863e04fbcd09bfe17c490a9e1f27
GangWoon <gangwoon2018@gmail.com>
2020-06-22 21:42:46 +0900

    [#62] feat: URLRequest init 파라미터 변경
    application/json을 고정 값으로 받고, method만 외부에서 전달하도록 변경했습니다.

--- iOS/IssueTracker/IssueTracker/Network/Endpoint.swift
+++ iOS/IssueTracker/IssueTracker/Network/Endpoint.swift
@@ -8,22 +8,21 @@
 
 import Foundation
 
-protocol RequestPorviding {
+protocol RequestProviding {
     var url: URL? { get }
-    
-    func request(_ method: String?, data: Data?, headers: [String: String]) throws -> URLRequest
 }
 
-struct Endpoint: RequestPorviding {
-    
+struct Endpoint: RequestProviding {
+
     enum Path: CustomStringConvertible {
-        case labels
+        case labels(String? = nil)
         case appleLogin
         
         var description: String {
             switch self {
-            case .labels:
-                return "/api/labels"
+            case let .labels(msg):
+                guard let msg = msg else { return "/api/labels" }
+                return "/api/labels/\(msg)"
             case .appleLogin:
                 return "/api/applelogin"
             }
@@ -36,25 +35,11 @@ struct Endpoint: RequestPorviding {
         components.host = baseUrl
         components.scheme = scheme
         components.path = path.description
-        
+
         return components.url
     }
     static let githubLogin: URL? = URL(string: "https://github.com/login/oauth/authorize?client_id=1aad2658e941ef024da5&scope=user%20public_repo")
     private let baseUrl: String = "13.209.115.251"
     private let scheme: String = "http"
     let path: Path
-    
-    // MARK: - Properties
-    func request(_ method: String? = "GET", data: Data?, headers: [String: String]) throws -> URLRequest {
-        guard let url = url else { throw IssueTrackerNetworkError.urlError }
-        var request = URLRequest(url: url)
-        request.httpMethod = method
-        request.httpBody = data
-        request.addValue("Bearer " + KeychainItem.currentUserIdentifier, forHTTPHeaderField: "Authorization")
-        headers.forEach { key, field in
-            request.addValue(key, forHTTPHeaderField: field)
-        }
-        
-        return request
-    }
 }
--- iOS/IssueTracker/IssueTracker/Supports/Extension/Foundation/URLRequest.swift
+++ iOS/IssueTracker/IssueTracker/Supports/Extension/Foundation/URLRequest.swift
@@ -9,28 +9,11 @@
 import Foundation
 
 extension URLRequest {
-    public enum HTTPMethod: String {
-        case get = "GET"
-        case put = "PUT"
-        case post = "POST"
-        case delete = "DELETE"
-    }
-    
-    public init(url: URL, method: HTTPMethod, contentType: String?, body: Data?) {
+    init(url: URL, method: HTTPMethod, body: Data? = nil) {
         self.init(url: url)
-        self.method = method
-        self.setValue(contentType, forHTTPHeaderField: "Content-Type")
+        self.httpMethod = method.description
+        self.setValue("application/json", forHTTPHeaderField: "Content-Type")
         self.setValue("Bearer " + KeychainItem.currentUserIdentifier, forHTTPHeaderField: "Authorization")
         self.httpBody = body
-    }
-    
-    public var method: HTTPMethod? {
-        get {
-            guard let httpMethod = self.httpMethod else { return nil }
-            let method = HTTPMethod(rawValue: httpMethod)
-            return method
-        } set {
-            self.httpMethod = newValue?.rawValue
-        }
     }
 }
