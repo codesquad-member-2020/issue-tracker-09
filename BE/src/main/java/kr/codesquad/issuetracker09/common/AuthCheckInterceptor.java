bcf72d4f9da6156f3d0d074f312d5d9517a6e0ed
suhyunsim <cnc1106@naver.com>
2020-06-25 14:30:16 +0900

    [#73] refactor: github과 apple 유저 구분해서 토큰 파싱
    - AuthCheckInterceptor에 custom header 추가해서 github과 apple 구분하도록 한다.
    - github 유저의 토큰의 경우 key를 통해 토큰을 파싱하고 apple 유저의 경우 key 없이 파싱한다.

--- BE/src/main/java/kr/codesquad/issuetracker09/common/AuthCheckInterceptor.java
+++ BE/src/main/java/kr/codesquad/issuetracker09/common/AuthCheckInterceptor.java
@@ -11,10 +11,12 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Slf4j
 public class AuthCheckInterceptor implements HandlerInterceptor {
     private final String HEADER_AUTH = "Authorization";
+    private final String LOGIN_TYPE = "LoginType";
     private final JwtService jwtService;
     private final UserService userService;
     private final UserRepository userRepository;
@@ -27,18 +29,22 @@ public class AuthCheckInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String loginType = request.getHeader(LOGIN_TYPE);
         String jwt = request.getHeader(HEADER_AUTH);
-
-        if (jwt != null) {
-            String socialId = jwtService.parseJwt(jwt).getSocialId();
-            log.debug("[*] socialId : {}", socialId);
-            User user = userRepository.findUserBySocialId(socialId).orElseThrow(NotFound::new);
-            log.debug("[*] user : {}", user);
-            log.debug("[*] userId : {}", user.getId());
-            request.setAttribute("id", user.getId());
-        } else {
+        if (jwt == null) {
             throw AuthorizationException.emptyToken();
         }
+        String socialId;
+        if (loginType.equals("github")) {
+            socialId = jwtService.parseGithubJwt(jwt).getSocialId();
+        } else if (loginType.equals("apple")) {
+            Map<String, Object> payloads = jwtService.getTokenPayload(jwt);
+            socialId = jwtService.parseAppleJwt(payloads).getSocialId();
+        } else {
+            throw new AuthorizationException("invalid token");
+        }
+        User user = userRepository.findUserBySocialId(socialId).orElseThrow(NotFound::new);
+        request.setAttribute("id", user.getId());
         return true;
     }
 }
