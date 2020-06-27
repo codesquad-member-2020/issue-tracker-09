635930b73dcccb91a54a8cd98430cffa56aa66d2
GangWoon <gangwoon2018@gmail.com>
2020-06-27 13:49:55 +0900

    [#77] feat: MileStone관련 파일 생성

---
+++ iOS/IssueTracker/IssueTracker/MileStone/MileStoneTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MileStoneTableViewCell.swift
+//  IssueTracker
+//
+//  Created by Cloud on 2020/06/27.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+final class MileStoneTableViewCell: UITableViewCell {
+    
+}
---
+++ iOS/IssueTracker/IssueTracker/MileStone/MileStoneTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MileStoneTableViewController.swift
+//  IssueTracker
+//
+//  Created by Cloud on 2020/06/27.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+final class MileStoneTableViewController: CategoryTableViewController {
+    
+}
