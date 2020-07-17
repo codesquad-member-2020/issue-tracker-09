b9255da14387e9e4423324efc478c1d0b9f7e91c
GangWoon <gangwoon2018@gmail.com>
2020-07-17 18:07:59 +0900

    [#90] refactor: LabelTableViewDataSource 수정

--- iOS/IssueTracker/IssueTracker/Label/LabelTableViewDataSource.swift
+++ iOS/IssueTracker/IssueTracker/Label/LabelTableViewDataSource.swift
@@ -12,8 +12,8 @@ import Combine
 final class LabelTableViewDataSource: NSObject {
     
     // MARK: - Properties
-    @Published var labels: [Label] = .init()
     private var subscription: Set<AnyCancellable> = .init()
+    @Published var labels: [Label] = .init()
 }
 
 // MARK: - Extension
@@ -26,8 +26,9 @@ extension LabelTableViewDataSource: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: LabelTableViewCell.identifier, for: indexPath) as? LabelTableViewCell else { return LabelTableViewCell() }
         let item = labels[indexPath.row]
-        cell.apply(title: item.title,
-                   description: item.contents ?? "",
+        cell
+            .apply(title: item.title,
+                   description: item.contents,
                    backgroundColor: UIColor(hex: item.colorCode) ?? UIColor.white)
         
         return cell
@@ -36,18 +37,18 @@ extension LabelTableViewDataSource: UITableViewDataSource {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete,
             let id = labels[indexPath.row].id else { return }
-            UseCase.shared
-                .encode(endpoint: Endpoint.init(path: .labels(String(id))),
-                        method: .delete)
-                .receive(on: RunLoop.main)
-                .sink(receiveCompletion: { _ in return }) { [weak self] response in
-                    switch response.statusCode {
-                    case 400 ..< 500:
-                        break
-                    default:
-                        self?.labels.remove(at: indexPath.row)
-                    }
-            }
-            .store(in: &subscription)
+        UseCase.shared
+            .encode(endpoint: Endpoint.init(path: .labels(String(id))),
+                    method: .delete)
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { _ in return }) { [weak self] response in
+                switch response.statusCode {
+                case 400 ..< 500:
+                    break
+                default:
+                    self?.labels.remove(at: indexPath.row)
+                }
+        }
+        .store(in: &subscription)
     }
 }
