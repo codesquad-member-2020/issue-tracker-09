6ced2d4fd5fc2cc562c8cfc7dda703e1fa853a4a
GangWoon <gangwoon2018@gmail.com>
2020-08-30 19:55:28 +0900

    [#108] feat: 프로토콜 Controllable 생성
    viewModel을 사용해서 서버에서 데이터를 받아오고 할당하는 메소드를 기본 구현으로 구현했습니다.

---
+++ iOS/IssueTracker/IssueTracker/Protocols/Controllable.swift
@@ -0,0 +1,37 @@
+//
+//  Controllable.swift
+//  IssueTracker
+//
+//  Created by Cloud on 2020/08/30.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+protocol Controllable: class {
+    associatedtype ViewModel: Modelable
+    static var identifier: String { get }
+    var headerViewTitle: String { get }
+    var viewModel: ViewModel { get set }
+    var cancellables: Set<AnyCancellable> { get set }
+}
+
+extension Controllable where Self: UIViewController {
+    func fetch(endpoint: RequestProviding) {
+        UseCase.shared
+            .fetch(type: [ViewModel.Item].self,
+                    endpoint: endpoint,
+                    method: .get)
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { [weak self] in
+                guard case let .failure(error) = $0 else { return }
+                let alertController = UIAlertController(message: error.message)
+                self?.present(alertController,
+                              animated: true)
+            }) { [weak self] items in
+                self?.viewModel.items = items
+        }
+        .store(in: &cancellables)
+    }
+}
