06b2065b1eb93d98a07951d4ef2f65fc4dbbe506
GangWoon <gangwoon2018@gmail.com>
2020-09-02 20:42:28 +0900

    [#108] feat: 프로토콜 FormControllable 생성
    FormVIewController에서 공통적으로 사용하는 기능을 프로토콜 기본 구현으로 처리했습니다.
    작성하면서 느낀점은 해당 프로퍼티를 담고 있는 객체에 접근하는게 아닌, 해당 프로토콜을 채택하고 있는 객체에 접근하는 시야를 확인했습니다.

---
+++ iOS/IssueTracker/IssueTracker/Protocols/FormControllable.swift
@@ -0,0 +1,88 @@
+//
+//  FormControllable.swift
+//  IssueTracker
+//
+//  Created by Cloud on 2020/09/02.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+protocol FormControllable: NSObject {
+    associatedtype Item: Hashable, Codable, Identifierable
+    var cancellables: Set<AnyCancellable> { get set }
+    var selectItem: Item? { get set }
+}
+
+extension FormControllable where Self: UIViewController {
+    func request<C>(controllable: C, item: Item, method: HTTPMethod) where C: Controllable, C: UIViewController, Item == C.ViewModel.Item {
+        UseCase.shared
+            .fetch(data: item,
+                   endpoint: Endpoint(path: generatePath(method: method, identity: item.id)),
+                   method: method)
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { [weak self] in
+                guard case let .failure(error) = $0 else { return }
+                let alertController = UIAlertController(message: error.message)
+                self?.present(alertController,
+                              animated: true)
+            }) { [weak self] _, response in
+                guard let statusCode = response?.statusCode else { return }
+                self?.checkStatusCode(controllable: controllable,
+                                      statusCode: statusCode,
+                                      method: method,
+                                      item: item)
+        }
+        .store(in: &cancellables)
+    }
+    
+    private func generatePath(method: HTTPMethod, identity: Int?) -> Endpoint.Path {
+        switch method {
+        case .post:
+            guard let _ = selectItem as? Label else {
+                return .mileStone()
+            }
+            
+            return .labels()
+        default:
+            guard let _ = selectItem as? Label else {
+                return .mileStone(String(identity ?? 0))
+            }
+            
+            return .labels(String(identity ?? 0))
+        }
+    }
+    
+    private func checkHTTPMethod<C>(controllable: C, method: HTTPMethod, updateItem: Item) where C: Controllable, C: UIViewController, Item == C.ViewModel.Item {
+        switch method {
+        case .post:
+            guard let _ = selectItem as? Label else {
+                controllable
+                    .fetch(endpoint: Endpoint(path: .mileStone()))
+                
+                return
+            }
+            controllable
+                .fetch(endpoint: Endpoint(path: .labels()))
+        default:
+            for (index, item) in controllable.viewModel.items.enumerated() {
+                _ = updateItem.id == item.id ? controllable.viewModel.items.remove(at: index) : nil
+                updateItem.id == item.id ? controllable.viewModel.items.insert(updateItem, at: index) : nil
+            }
+        }
+    }
+    
+    private func checkStatusCode<C>(controllable: C, statusCode: Int, method: HTTPMethod, item: Item) where C: Controllable, C: UIViewController, Item == C.ViewModel.Item {
+        switch statusCode {
+        case 200 ..< 300:
+            checkHTTPMethod(controllable: controllable,
+                            method: method,
+                            updateItem: item)
+        default:
+            let alert = UIAlertController(message: "Network Error")
+            present(alert,
+                    animated: true)
+        }
+    }
+}
